4bf27e14d42d62cfc18a1bfccc4940d52defde88
princesssuna <1009suna@gmail.com>
2021-11-03 23:16:47 +0900

    수학, 문자열, 사칙연산

---
+++ 11720_숫자의 합.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int n=Integer.parseInt(br.readLine());
+        String num=br.readLine();
+        int count=0;
+
+        for(int i=0; i<n; i++) {
+            count+=num.charAt(i)-'0';
+        }
+
+        System.out.println(count);
+    }
+}
